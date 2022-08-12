4d6a1e70c8eae42933f20c48553ede531fbf49dc
luisdy1972 <luisdy1972@hotmail.com>
2022-08-12 11:03:51 -0500

    UC

--- js/prueba.js
+++ js/prueba.js
@@ -1,5 +1,16 @@
 function pruebasBT() {
-    document.write();
+  // tarjera
+  document.write(
+`<div class="col-sm-12 card shadow">
+
+      <div class="card-body">
+        <h5 class="card-title">Pruebas</h5>
+        <p class="card-text">Tarjeta de pruebas</p>
+        <a href="https://www.jw.org"
+          rel="noopener noreferrer" target="_blank" class="btn btn-primary">Jw.org</a>
+      </div>
+
+    </div>`);
 }
 
 pruebasBT();
