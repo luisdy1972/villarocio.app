a53d078b521e0a5ec248ae577c272c174b6bde4e
Luis_Diego_Ayala <luisdy1972@hotmail.com>
2023-02-24 19:06:19 -0500

    Init-con-Vue-Bootstrap5

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/js/bootstrap";
+
+// import "./assets/main.css";
+import "./assets/css/bootstrap.css";
+
+createApp(App).mount("#app");
