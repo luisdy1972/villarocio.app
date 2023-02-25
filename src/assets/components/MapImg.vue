f1c5bac14020545891dbd63f3bdc75db0641edc9
luisdy1972 <luisdy1972@hotmail.com>
2023-02-25 11:08:14 -0500

    25-02-2023-11am

---
+++ src/assets/components/MapImg.vue
@@ -0,0 +1,53 @@
+<script></script>
+<template setup>
+	<div>
+		<!-- <div class="card mb-3">
+			<img
+				src="../../../public/img/TargetasTerritorio(Obtimizado).jpg"
+				class="card-img-top img-territorio"
+				alt="Mapa dividido por tarjetas de la congregación"
+			/>
+			<div class="card-body">
+				<h5 class="card-title">Territorio</h5>
+				<p class="card-text">
+					Mapa dividido por tarjetas de la congregación Villa Rocío
+				</p>
+				<p class="card-text">
+					<small class="text-muted">Actualizado en 2019</small>
+				</p>
+			</div>
+		</div> -->
+		<div class="container">
+			<div class="card mb-3" style="max-width: 540px">
+				<div class="row g-0">
+					<div class="col-md-4">
+						<img
+							src="../../../public/img/TargetasTerritorio(Obtimizado).jpg"
+							class="img-fluid rounded-start"
+							alt="..."
+						/>
+					</div>
+					<div class="col-md-8">
+						<div class="card-body">
+							<h5 class="card-title">Territorio</h5>
+							<p class="card-text">
+								Ver y descargar imagen de todo el mapa completo del Territorio
+								de la congregación
+							</p>
+							<p class="card-text">
+								<small class="text-muted">Actualizado en 2019</small>
+							</p>
+
+							<a href="#" class="card-link">Ver</a>
+							<a href="#" class="card-link">Descargar</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<style scoped>
+.img-territorio {
+}
+</style>
---
+++ src/assets/components/NavBar.vue
@@ -0,0 +1,73 @@
+<script setup></script>
+
+<template>
+	<div>
+		<nav class="navbar bg-body-tertiary fixed-top shadow">
+			<div class="container-fluid">
+				<a class="titulo-principal" href="#">
+					<h1>Villa Rocío</h1>
+				</a>
+				<a class="" href="#">
+					<img class="logoVR" src="../../../public/img/favicon.png" />
+				</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="offcanvas"
+					data-bs-target="#offcanvasNavbar"
+					aria-controls="offcanvasNavbar"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div
+					class="offcanvas offcanvas-end"
+					tabindex="-1"
+					id="offcanvasNavbar"
+					aria-labelledby="offcanvasNavbarLabel"
+				>
+					<div class="offcanvas-header">
+						<h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+							Herramientas
+						</h5>
+						<button
+							type="button"
+							class="btn-close"
+							data-bs-dismiss="offcanvas"
+							aria-label="Close"
+						></button>
+					</div>
+					<div class="offcanvas-body">
+						<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+							<li class="nav-item">
+								<a class="nav-link active" aria-current="page" href="#"
+									>Territorio</a
+								>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Arreglos de conferencias</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</div>
+</template>
+
+<style scoped>
+.logoVR {
+	margin: 0;
+	padding: 0;
+	height: 2.5rem;
+}
+h1 {
+	margin: 0;
+	padding: 0;
+	font-size: 2rem;
+}
+.titulo-principal {
+	margin: 0;
+	color: black;
+	text-decoration: none;
+}
+</style>
