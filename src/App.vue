02fe2128a0f46204c232820a2fcedf56d94e0194
Luis Ayala <luisdy1972@hotmail.com>
2023-07-14 16:19:08 -0500

    Listado-grupos-ok

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
+// modulos
 import { ref, onMounted } from "vue"
 
+// componentes
 import NavBar from "./assets/components/NavBar.vue"
 import Carousel from "./assets/components/Carousel.vue"
+import CardPerson from "./assets/components/CardPerson.vue"
 
+// librerias
 import axios from "axios"
 const rutaData = "https://villarocio-ba5a1-default-rtdb.europe-west1.firebasedatabase.app/.json";
 
@@ -22,7 +26,6 @@ onMounted(() => {
       console.error(error)
     })
 })
-import CardPerson from "./assets/components/CardPerson.vue"
 </script>
 
 <template>
@@ -31,12 +34,12 @@ import CardPerson from "./assets/components/CardPerson.vue"
     <Carousel class="mb-5" />
     <section class="container">
       <h2 class="p-4">Gupos</h2>
+      <div class="d-flex justify-content-center flex-wrap mb-5">
+        <CardPerson class="m-1" v-for="anciano in ancianos" :grupo="anciano.numero" :nombre="anciano.nombre"
+          :responsabilidad="anciano.responsabilidad" :linkInforme="anciano.linkInforme"
+          :linkAgenda="anciano.linkAgenda" />
+      </div>
     </section>
-    <div class="container mb-5">
-      <!-- :for -->
-      <CardPerson :grupo="ancianos[0].numero" :nombre="ancianos[0].nombre" :responsabilidad="ancianos[0].responsabilidad"
-        :linkInforme="ancianos[0].linkInforme" :linkAgenda="ancianos[0].linkAgenda" />
-    </div>
   </div>
 </template>
 
--- src/assets/components/CardPerson.vue
+++ src/assets/components/CardPerson.vue
@@ -14,8 +14,8 @@ const props = defineProps({
     <div class="card-body">
       <h5 class="card-title">{{ props.nombre }}</h5>
       <p class="card-text">{{ props.responsabilidad }}</p>
-      <a :href="props.linkInforme" class="btn btn-primary m-1">Informe</a>
-      <a :href="props.linkAgenda" class="btn btn-primary m-1">Agenda</a>
+      <a :href="props.linkInforme" target="_blank" class="btn btn-primary m-1">Informe</a>
+      <a :href="props.linkAgenda" target="_blank" class="btn btn-primary m-1">Agenda</a>
     </div>
   </div>
 </template>
@@ -23,5 +23,6 @@ const props = defineProps({
 <style scoped>
 .card-person {
   max-width: 400px;
+  min-width: 250px;
 }
 </style>
