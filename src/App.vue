3aaaabb657dea3cbe9a3937ce5fd31e0727c52c4
Luis Ayala <luisdy1972@hotmail.com>
2023-07-15 13:39:54 -0500

    ok

--- src/App.vue
+++ src/App.vue
@@ -6,27 +6,55 @@ import { ref, onMounted } from 'vue'
 import NavBar from './assets/components/NavBar.vue'
 import Carousel from './assets/components/Carousel.vue'
 import CardGup from './assets/components/CardGup.vue'
+import CardPerson from './assets/components/CardPerson.vue'
 
 // librerias
 import axios from 'axios'
-const rutagrupos =
-	'https://villarocio-ba5a1-default-rtdb.europe-west1.firebasedatabase.app/grupos.json'
+const rutas = {
+	grupos:
+		'https://villarocio-ba5a1-default-rtdb.europe-west1.firebasedatabase.app/grupos.json',
+	ancianos:
+		'https://villarocio-ba5a1-default-rtdb.europe-west1.firebasedatabase.app/ancianos.json',
+	ministeriales:
+		'https://villarocio-ba5a1-default-rtdb.europe-west1.firebasedatabase.app/ministeriales.json',
+}
 
 const grupos = ref([''])
-// const ministeriales = ref([''])
+const ancianos = ref([''])
+const ministeriales = ref([''])
 
 onMounted(() => {
 	axios
-		.get(rutagrupos)
+		.get(rutas.grupos)
 		.then(function (response) {
 			grupos.value = response.data
 			// console.log(response.data)
-			console.log(grupos.value)
+			// console.log(grupos.value)
 		})
 		.catch(function (error) {
 			console.error(error)
 		})
+
+	axios
+		.get(rutas.ancianos)
+		.then(function (res) {
+			ancianos.value = res.data
+			// console.log(ancianos.value)
+		})
+		.catch(function (err) {
+			console.error(err)
+		})
 })
+
+axios
+	.get(rutas.ministeriales)
+	.then(function (res) {
+		ministeriales.value = res.data
+		console.log(ministeriales.value)
+	})
+	.catch(function (err) {
+		console.error(err)
+	})
 </script>
 
 <template>
@@ -47,10 +75,26 @@ onMounted(() => {
 		</section>
 		<section class="container">
 			<h2 class="p-4">Arreglos de conferencias</h2>
-			<p>En proceso...</p>
+			<div class="d-flex justify-content-center flex-wrap mb-5">
+				<CardPerson
+					class="m-1"
+					v-for="anciano in ancianos"
+					:nombre="anciano.nombre"
+					:responsabilidad="'Anciano'"
+					:linkAgenda="anciano.linkAgenda"
+				/>
+				<div class="d-flex justify-content-center flex-wrap mb-5">
+					<CardPerson
+						class="m-1"
+						v-for="minisretial in ministeriales"
+						:nombre="minisretial.nombre"
+						:responsabilidad="'Ministerial'"
+						:linkAgenda="minisretial.linkAgenda"
+					/>
+				</div>
+			</div>
 		</section>
 	</div>
-	<div></div>
 </template>
 
 <style scoped>
