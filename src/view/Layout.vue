0745bf861937ec137cd3fc161bde34097ec3292e
Luisdy1972 <luisdy1972@hotmail.com>
2023-10-14 09:40:38 -0500

    ajustando-footer

--- src/view/Layout.vue
+++ src/view/Layout.vue
@@ -58,7 +58,7 @@
 	<div style="margin-top: 4rem">
 		<RouterView></RouterView>
 	</div>
-	<footer class="bd-footer py-4 py-md-5 mt-5 bg-body-tertiary">
+	<!-- <footer class="bd-footer py-4 py-md-5 mt-5 bg-body-tertiary">
 		<div class="container py-4 py-md-5 px-4 px-md-3 text-body-secondary">
 			<div class="d-flex justify-content-between">
 				<div class="">
@@ -100,14 +100,56 @@
 				</div>
 			</div>
 		</div>
+	</footer> -->
+	<footer class="bg-body-tertiary pt-3">
+		<div class="container text-body-secondary">
+			<div class="d-flex justify-content-between">
+				<div class="">
+					<a
+						class="d-inline-flex align-items-center mb-2 text-body-emphasis text-decoration-none"
+						href="/"
+						aria-label="Bootstrap"
+					>
+						<span class="fs-5">Villa Rocío</span>
+					</a>
+					<ul class="list-unstyled small">
+						<li class="mb-2 parrafo-futher">
+							En este sitio web, podrá: <br />
+							Visualizar y descargar el mapa del territorio general. Ver una
+							lista de grupos. Revisar agendas personales de conferencias. Ver
+							acuerdos de conferencias con otras congregaciones.
+						</li>
+					</ul>
+				</div>
+				<div class="">
+					<h5>Índice</h5>
+					<ul class="list-unstyled">
+						<li class="">
+							<a href="/">Principal</a>
+						</li>
+						<li class="text-decoration-none">
+							<RouterLink to="/grupos">Grupos</RouterLink>
+						</li>
+						<li class="text-decoration-none">
+							<RouterLink to="/agenda">Agendas</RouterLink>
+						</li>
+						<li class="text-decoration-none">
+							<RouterLink to="/agenda/ancianos">Ancianos</RouterLink>
+						</li>
+						<li class="text-decoration-none">
+							<RouterLink to="/agenda/ministeriales">Ministeriales</RouterLink>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</footer>
 </template>
 
 <style scoped>
-.logoVR {
-	margin: 0;
-	padding: 0;
-	height: 2.5rem;
+li > a {
+	text-decoration: none;
+	color: gray;
 }
 
 h1 {
@@ -116,15 +158,19 @@ h1 {
 	font-size: 2rem;
 }
 
-.titulo-principal {
-	margin: 0;
-	color: black;
-	text-decoration: none;
-}
 .boton-navegacion {
 	color: #62bee8;
 }
 .parrafo-futher {
-	max-width: 20%;
+	max-width: 80%;
+}
+.logoVR {
+	margin: 0;
+	padding: 0;
+	height: 2.5rem;
+}
+
+.boton-navegacion {
+	color: #62bee8;
 }
 </style>
