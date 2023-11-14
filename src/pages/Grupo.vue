427ea7fd0bcb2df679c5d80a08427822cd73c253
Luisdy1972 <luisdy1972@hotmail.com>
2023-11-14 16:43:59 -0500

    add-form

--- src/pages/Grupo.vue
+++ src/pages/Grupo.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
-import { user, db, buscarDocumentos, guardarActualizarDocumento } from '@db'
+import { user, buscarDocumentos, guardarActualizarDocumento } from '@db'
+
+import { Loading, Notification } from '@components'
 
 const { params } = useRoute()
 const publicadores = ref([])
@@ -23,7 +25,7 @@ function buscarPublicadoresPorGrupo(numeroGrupo) {
 	// console.log('Grupo: ' + Number(numeroGrupo))
 	buscarDocumentos('publicadores', ['grupo', '==', Number(numeroGrupo)])
 		.then((res) => {
-			console.log(res)
+			// console.log(res)
 			publicadores.value = res
 		})
 		.catch((err) => {
@@ -31,7 +33,30 @@ function buscarPublicadoresPorGrupo(numeroGrupo) {
 		})
 }
 
-function agregarPublicador() {}
+function agregarPublicador() {
+	// console.log(nuevoPublicador.value)
+	guardarActualizarDocumento('publicadores', nuevoPublicador.value)
+		.then((result) => {
+			publicadores.value = []
+			nuevoPublicador.value = {
+				nombre: '',
+				bautizado: false,
+				anciano: false,
+				auxiliar: false,
+				regular: false,
+				horas: 0,
+				comentario: '',
+				estudios: 0,
+				grupo: Number(params.numero),
+				informe: false,
+				ministerial: false,
+			}
+			buscarPublicadoresPorGrupo(params.numero)
+		})
+		.catch((err) => {
+			console.error(err)
+		})
+}
 
 onMounted(() => {
 	buscarPublicadoresPorGrupo(params.numero)
@@ -39,21 +64,68 @@ onMounted(() => {
 </script>
 <template>
 	<div class="container pt-5">
-		<ul id="listado-grupo" class="list-group mb-5">
-			<div class="list-group-item active">
-				<b> Grupo Número {{ params.numero }}</b>
-			</div>
-			<div class="list-group-item"><b>Publicadores</b></div>
-			<li
-				v-for="publicador in publicadores"
-				class="list-group-item list-group-item-action ps-4"
-			>
-				{{ publicador.nombre }}
-			</li>
-			<div class="list-group-item"><b>Precursores Auxiliares</b></div>
-			<div class="list-group-item"><b>Precursores Regulares</b></div>
-			<div class="list-group-item"><b>Precursores Especiales</b></div>
-		</ul>
+		<h3 class="active">
+			<b> Grupo Número {{ params.numero }}</b>
+		</h3>
+		<Loading v-if="publicadores.length == 0"></Loading>
+
+		<table
+			v-else
+			class="table table-responsive table-sm mt-3 table-bordered table-hover"
+		>
+			<thead class="table-primary">
+				<tr>
+					<!-- <th scope="col">#</th> -->
+					<th scope="col">Publicadores</th>
+					<th scope="col">¿Participó en la predicación?</th>
+					<th scope="col">Estudios</th>
+					<th scope="col">Comentario</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr scope="row" v-for="publicador in publicadores">
+					<!-- <td>{{}}</td> -->
+					<td>
+						{{ publicador.nombre }}
+					</td>
+					<td class="input-group-sm">
+						<input
+							class="form-check-input"
+							type="checkbox"
+							:value="publicador.informe"
+							@change=""
+						/>
+					</td>
+					<td>
+						<input
+							type="number"
+							:value="publicador.estudios"
+							class="form-control"
+						/>
+					</td>
+					<td>
+						<input
+							class="form-control"
+							type=""
+							:value="publicador.comentario"
+							@change=""
+						/>
+					</td>
+				</tr>
+			</tbody>
+			<tfoot class="table-primary">
+				<tr>
+					<th>Total</th>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</tfoot>
+		</table>
+
+		<div class="list-group-item"><b>Precursores Auxiliares</b></div>
+		<div class="list-group-item"><b>Precursores Regulares</b></div>
+		<div class="list-group-item"><b>Precursores Especiales</b></div>
 
 		<!-- agregar Publicador -->
 		<div class="container d-flex justify-content-center">
@@ -70,8 +142,7 @@ onMounted(() => {
 							type="text"
 							class="form-control"
 							id="nombre"
-							required
-							:model="nuevoPublicador.nombre"
+							v-model="nuevoPublicador.nombre"
 						/>
 					</div>
 					<div class="ms-3">
@@ -80,7 +151,7 @@ onMounted(() => {
 							type="checkbox"
 							class="form-check-input ms-1"
 							id="bautizado"
-							:model="nuevoPublicador.bautizado"
+							v-model="nuevoPublicador.bautizado"
 						/>
 						<div>
 							<div>
@@ -101,7 +172,7 @@ onMounted(() => {
 										type="radio"
 										name="precursor"
 										id="auxiliar"
-										:model="nuevoPublicador.auxiliar"
+										v-model="nuevoPublicador.auxiliar"
 									/>
 								</div>
 								<div>
@@ -111,7 +182,7 @@ onMounted(() => {
 										type="radio"
 										id="regular"
 										name="precursor"
-										:model="nuevoPublicador.regular"
+										v-model="nuevoPublicador.regular"
 									/>
 								</div>
 							</div>
@@ -123,7 +194,7 @@ onMounted(() => {
 									class="form-check-input ms-1"
 									type="checkbox"
 									id="ministerial"
-									:model="nuevoPublicador.ministerial"
+									v-model="nuevoPublicador.ministerial"
 								/>
 							</div>
 							<div>
@@ -132,7 +203,7 @@ onMounted(() => {
 									class="form-check-input ms-1"
 									type="checkbox"
 									id="anciano"
-									:model="nuevoPublicador.anciano"
+									v-model="nuevoPublicador.anciano"
 								/>
 							</div>
 						</div>
